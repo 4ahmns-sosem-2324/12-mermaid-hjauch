499de4b1006550a238fc7eb2aa4537054615c05f
henrik0314 <heja.1709@gmail.com>
2024-02-28 09:50:36 +0100

    Erstellen von Scripts

---
+++ Assets/MyGame/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    float chaseSpeed;
+    Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyGame/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    GameObject player;
+    GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
